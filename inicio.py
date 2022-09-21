eb197cf1952bab20411c9865585fdd1469943647
Javier Miles <Javier@Miles>
2022-09-21 20:25:27 -0300

    Init commit

---
+++ inicio.py
@@ -0,0 +1,10 @@
+#Esto es un comentario
+from shutil import unpack_archive
+
+
+#ENTRADA
+unavariable = input("Escribe un valor")
+#PROCESO
+unavariable = unavariable + "PROCESANDO DATOS"
+#SALIDA
+print("Hola mundo"+unavariable)
